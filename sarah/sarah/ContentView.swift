b35ea1a1d88a762226bd8ec58d513b7406008821
sarah almutari <sara0moha78@gmail.com>
2022-03-02 19:43:00 +0300

    added my name

---
+++ sarah/sarah/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sarah
+//
+//  Created by sarah almutari on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello im sarah")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sarah/sarah/sarahApp.swift
@@ -0,0 +1,17 @@
+//
+//  sarahApp.swift
+//  sarah
+//
+//  Created by sarah almutari on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct sarahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
