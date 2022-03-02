47d2cee26af92c9c6d0a8cf318783607f501888b
sarah almutari <sara0moha78@gmail.com>
2022-03-02 20:05:06 +0300

    added my name

---
+++ xuvernon/xuvernon/xuvernonApp.swift
@@ -0,0 +1,17 @@
+//
+//  xuvernonApp.swift
+//  xuvernon
+//
+//  Created by sarah almutari on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct xuvernonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
